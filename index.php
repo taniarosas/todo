5ba699cc80062fa78da8eabfd17277a049749660
tania rosas-jimenez <rosastania@yahoo.com>
2015-04-14 11:16:37 -0700

    finished video 2

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Simple To-Do List</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
-
+	<div class="wrap">
+			
+	</div>
 </body>
 </html>
