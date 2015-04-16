33a548a07e8940bf1a8e3fb92a9dc61f5ee261d3
tania rosas-jimenez <rosastania@yahoo.com>
2015-04-15 19:35:58 -0700

    added ; to index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 				$.post('includes/add-task.php', { task: new_task}, function(data) {
 					$('add-new-task input[name=new-task]').val();
 					$(data).appendTo('task-list ul').hide().fadeIn();
-				})
+				});
 			}
 			return false;
 		});
